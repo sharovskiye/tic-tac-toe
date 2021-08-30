92cf5f4d23c0d19defa2b4684e64a34e31837ce5
Yevhenii <sharovskiyevgeniy@gmail.com>
2021-08-31 01:42:44 +0300

    feat: complete task

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -2,17 +2,30 @@ class TicTacToe {
     constructor() {
 
     }
+    player='x'
 
+    matrix=[
+        [null,null,null],
+        [null,null,null],
+        [null,null,null]
+    ]
     getCurrentPlayerSymbol() {
-
+        return this.player
     }
 
     nextTurn(rowIndex, columnIndex) {
-
+        // console.log(this.matrix[rowIndex][columnIndex]);
+        if(!this.matrix[rowIndex][columnIndex]){
+        this.matrix[rowIndex][columnIndex]=this.player
+        this.player=this.player=='x'?'o':'x'}
+        // if(this.player==='x'){
+        //     this.player='o'
+        // }else { this.player='x'}
+        // console.log(this.player);
     }
 
     isFinished() {
-
+        
     }
 
     getWinner() {
@@ -20,16 +33,30 @@ class TicTacToe {
     }
 
     noMoreTurns() {
-
+        for (const elem of this.matrix) {
+            for (const item of elem) {
+                if (item===null) {
+                    return false
+                    
+                }   
+            }
+        }
+       
+        return true
     }
 
     isDraw() {
 
     }
 
-    getFieldValue(rowIndex, colIndex) {
-
+    getFieldValue(rowIndex, columnIndex) {
+        return this.matrix[rowIndex][columnIndex]
     }
 }
+//  game= new TicTacToe()
+// console.log(game.getCurrentPlayerSymbol());
+// game.nextTurn(1,1)
+// console.log(game.getCurrentPlayerSymbol());
+
 
 module.exports = TicTacToe;
